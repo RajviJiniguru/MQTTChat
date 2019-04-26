c8472528ef2d994a4004656bea8a7f97c1cdbee0
rajviturakhia <rajvi.turakhia@jini.guru>
2019-04-26 15:50:17 +0530

    Initial Commit

---
+++ MQTTSDK/MQTTSDK.h
@@ -0,0 +1,19 @@
+//
+//  MQTTSDK.h
+//  MQTTSDK
+//
+//  Created by Parth Adroja on 26/04/19.
+//  Copyright © 2019 Rajvi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MQTTSDK.
+FOUNDATION_EXPORT double MQTTSDKVersionNumber;
+
+//! Project version string for MQTTSDK.
+FOUNDATION_EXPORT const unsigned char MQTTSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MQTTSDK/PublicHeader.h>
+
+
